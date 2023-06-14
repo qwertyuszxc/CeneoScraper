b75535ee9d8a557279b16e765d71bb79123e5099
qwertyuszxc <225270@student.uek.krakow.pl>
2023-06-14 12:49:13 +0200

    14-06-2023 NS

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_form
 
 @app.route("/")
 def index():
@@ -10,14 +10,23 @@ def index():
 def name(name):
     return f"Hello {name}"
 
-@app.route("/o_autorze")
+@app.route("/o_autorze") 
 def author():
     return render_template('about_author.html')
 
-@app.route("/ekstrakcja")
+@app.route("/ekstrakcja", method =['POST','GET'])
 def extraction():
+    if request.method == 'POST':
+        product_code = request.form.get['product_id']
+        
+        return redirect(url_for{'product',product_code=product_id})
     return render_template('extraction.html')
+
+@app.route("/ekstrakcja/<product_code>")
+def extraction(product_code):
+    return render_temlate(extraction.html)   
 
 @app.route("/lista_produktow")
 def productList():
     return render_template('product_list.html')
+    
---
+++ app/utils.py
@@ -0,0 +1,11 @@
+def get_element(ancestor, selector = None, attribute = None, return_list = False):
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
+        if not selector and attribute:
+            return ancestor[attribute]
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).text.strip()
+    except (AttributeError,TypeError):
+        return None
