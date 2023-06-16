940392f42e58caf7ff9031b8587487b96116ca80
qwertyuszxc <225270@student.uek.krakow.pl>
2023-06-16 19:47:07 +0200

    06-16-2023 NS fix

--- app/routes.py
+++ app/routes.py
@@ -14,17 +14,36 @@ def name(name):
 def author():
     return render_template('about_author.html')
 
-@app.route("/ekstrakcja", method =['POST','GET'])
-def extraction():
-    if request.method == 'POST':
-        product_code = request.form.get['product_id']
-        
-        return redirect(url_for{'product',product_code=product_id})
-    return render_template('extraction.html')
+@app.route('/ekstrakcja', methods=['POST', 'GET'])
+def extract():
+    if request.method == "POST":
+        product_code = request.form.get('product_code')
+        url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+        all_opinions =[]
+        while(url):
+            print(url)
+            response = requests.get(url)
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            opinions = page_dom.select("div.js_product-review")
+            for opinion in opinions:
+                single_opinion = {}
+                for key, value in selectors.items():
+                    single_opinion[key] = extract_tag(opinion, *value)
+                all_opinions.append(single_opinion)
+            try:
+                url = "https://www.ceneo.pl" + extract_tag(page_dom, "a.pagination__next", "href")
+            except TypeError:
+                url = None
+        with open(f"./app/data/opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+            json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', product_code=product_code))
+    return render_template('extract.html')
 
-@app.route("/ekstrakcja/<product_code>")
-def extraction(product_code):
-    return render_temlate(extraction.html)   
+
+@app.route('/product/<product_code>')
+def product(product_code):
+    opinions = pd.read_json(f"./app/data/opinions/{product_code}.json")
+    return render_template('product.html', product_code=product_code, opinions=opinions.to_html(header=1, classes='table table-striped table-success', table_id='opinions')) 
 
 @app.route("/lista_produktow")
 def productList():
--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,26 @@
-def get_element(ancestor, selector = None, attribute = None, return_list = False):
+def extract_tag(ancestor, selector=None, attribute=None, return_list=False):
     try:
         if return_list:
-            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
+            return [tag.text.strip() for tag in ancestor.select(selector)]
         if not selector and attribute:
             return ancestor[attribute]
         if attribute:
             return ancestor.select_one(selector)[attribute].strip()
         return ancestor.select_one(selector).text.strip()
-    except (AttributeError,TypeError):
+    except (AttributeError, TypeError):
         return None
+
+selectors = {
+    "opinion_id": [None, "data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recommendation":["span.user-post__author-recomendation > em"],
+    "rating": ["span.user-post__score-count"],
+    "verified": ["div.review-pz"],
+    "post_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "vote_up": ["button.vote-yes", "data-total-vote"],
+    "vote_down": ["button.vote-no", "data-total-vote"],
+    "content": ["div.user-post__text"],
+    "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True],
+    "pros": ["div.review-feature__title--positives ~ div.review-feature__item", None, True]
+}
